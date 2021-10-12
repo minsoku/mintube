08459b85c12e3fae37689ebe609b396e2b21165e
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-10-12 23:38:23 +0900

    router 파일 나누기

--- src/server.js
+++ src/server.js
@@ -1,27 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 app.use(morgan("dev"));
 
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("home");
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("edit user");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("watch video");
-videoRouter.get("/watch", handleWatchVideo);
-
 app.use("/", globalRouter);
-
 app.use("/videos", videoRouter);
-
 app.use("/user", userRouter);
 
 const handleListening = () => console.log(`server ${PORT}`);
