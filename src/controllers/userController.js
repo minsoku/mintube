1ec98201013d197fe663c5b52ba4658c311bf44e
민석 / MIN / ミンソク <minsoku03@gmail.com>
2021-10-13 23:36:26 +0900

    원상복귀

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,3 @@
-export const join = (req, res) => res.send("joinn");
+export const join = (req, res) => res.send("join");
 export const edit = (req, res) => res.send("edit");
 export const deleteUser = (req, res) => res.send("delete");
