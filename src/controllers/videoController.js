8828b613e5451a51fa4eb9959267a09a104cd3dd
민석 / MIN / ミンソク <minsoku03@gmail.com>
2021-10-14 23:48:18 +0900

    parameter study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
+export const watch = (req, res) => res.send("watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
