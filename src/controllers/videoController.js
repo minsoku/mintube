0f6bcd83b1b2436be9e555b31b52b56521b9f62e
민석 / MIN / ミンソク <minsoku03@gmail.com>
2021-10-15 00:09:16 +0900

    params id 에 숫자만 들어갈 수 있는 정규식 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,7 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("watch");
+export const watch = (req, res) => {
+  return res.send(req.params.id);
+};
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
