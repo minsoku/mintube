b24debc89c920c607ea63cbf88584c81b80886aa
민석 / MIN / ミンソク <minsoku03@gmail.com>
2021-12-14 00:27:37 +0900

    Controlloer 에서 header에 넣을 변수 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const watch = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "home" });
+export const watch = (req, res) => res.render("watch", { pageTitle: "watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("deleteVideo");
