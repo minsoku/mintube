99dc535d9a36f239d2c050006d3db80da6999190
민석 / MIN / ミンソク <minsoku03@gmail.com>
2021-12-20 23:54:36 +0900

    pug 앵커 태그 추가해서 id value 받아봄

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,37 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 1,
+    comments: 1,
+    createdAt: "1 minutes ago",
+    views: 11,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 2,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 22,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 3,
+    comments: 3,
+    createdAt: "3 minutes ago",
+    views: 33,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 1,
-      comments: 1,
-      createdAt: "1 minutes ago",
-      views: 11,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 2,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 22,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 3,
-      comments: 3,
-      createdAt: "3 minutes ago",
-      views: 33,
-      id: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => res.render("watch", { pageTitle: "watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watch ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
