4a0c195dde7b933362e1ad06be975ca857b4839a
민석 / MIN / ミンソク <minsoku03@gmail.com>
2021-12-20 23:23:57 +0900

    기본 템플릿 완료

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,32 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "home" });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 1,
+      comments: 1,
+      createdAt: "1 minutes ago",
+      views: 11,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 2,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 22,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 3,
+      comments: 3,
+      createdAt: "3 minutes ago",
+      views: 33,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const watch = (req, res) => res.render("watch", { pageTitle: "watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
 export const search = (req, res) => res.send("search");
