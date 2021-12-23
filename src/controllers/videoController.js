60204150f710422ac4098e509a93e328e86245d2
민석 / MIN / ミンソク <minsoku03@gmail.com>
2021-12-23 22:26:31 +0900

    use fake db and make video file

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,22 +26,44 @@ let videos = [
     id: 3,
   },
 ];
+
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("watch", { pageTitle: `Watch ${video.title}`, video });
 };
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("edit", { pageTitle: `Edit , ${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload" });
+};
+
+// 비디오 배열에 추가 할 거임
+export const postUpload = (req, res) => {
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "1분전",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
