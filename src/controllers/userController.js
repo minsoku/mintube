544d79e1fb1942abe95e9e60c6fb33e96c4a0641
민석 / MIN / ミンソク <minsoku03@gmail.com>
2022-01-13 00:54:23 +0900

    mongodb 연동해서 create 기능 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,16 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {
-  console.log(req.body);
+export const postJoin = async (req, res) => {
+  const { email, username, name, password, location } = req.body;
+  await User.create({
+    email,
+    username,
+    name,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("edit");
 export const deleteUser = (req, res) => res.send("delete");
