633c76bf5451d05ba000064d51e68d88693cd88e
민석 / MIN / ミンソク <minsoku03@gmail.com>
2022-01-13 00:02:29 +0900

    globalRouter => rootRouter 이름 변경 및 user model 파일 생성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
-export const join = (req, res) => res.send("join");
+export const join = (req, res) =>
+  res.render("createAccount", { pageTitle: "createAccount" });
 export const edit = (req, res) => res.send("edit");
 export const deleteUser = (req, res) => res.send("delete");
 export const login = (req, res) => res.send("login");
