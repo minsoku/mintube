67476fb4be1419677cf326a060d81e1112c32385
민석 / MIN / ミンソク <minsoku03@gmail.com>
2022-01-18 23:12:52 +0900

    status code 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,13 +5,13 @@ export const postJoin = async (req, res) => {
   const { email, username, name, password, password2, location } = req.body;
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle: "Join",
       errorMessage: "Password confirmation does not match ",
     });
   }
   if (exists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle: "Join",
       errorMessage: "This username/email is already taken",
     });
